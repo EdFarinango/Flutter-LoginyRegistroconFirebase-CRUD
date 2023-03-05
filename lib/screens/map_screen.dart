3ea7c85034ec4eaad03c96f5099608df9c7aff37
EdFarinango <eduardo.farinango@epn.edu.ec>
2023-03-05 16:18:42 -0500

    manejo se sesión,

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_login_register/login.dart';
+import 'package:flutter_login_register/src/ui/startScreen.dart';
+import 'package:flutterfire_ui/auth.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -15,7 +18,7 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends State<MapScreen> {
   final Map<String, Marker> _markers = {};
-  late LatLng _currentLocation;
+  late LatLng _currentLocation = LatLng(0, 0);
   bool _isLoading = false;
   late String _loggedInUserEmail;
 
@@ -26,8 +29,7 @@ class _MapScreenState extends State<MapScreen> {
         .map((snapshot) {
       return snapshot.docs.map((doc) {
         final userId = doc.id;
-    
-      
+
         final geoPoint = doc['location'] as GeoPoint;
         final location = LatLng(geoPoint.latitude, geoPoint.longitude);
 
@@ -37,8 +39,6 @@ class _MapScreenState extends State<MapScreen> {
           infoWindow: InfoWindow(
             title: userId,
             snippet: location.toString(),
-
-
           ),
         );
       }).toSet();
@@ -50,6 +50,14 @@ class _MapScreenState extends State<MapScreen> {
     super.initState();
     _getUserLocation();
     _getUserEmail();
+    _checkIfUserExists(FirebaseAuth.instance.currentUser!.uid).then((exists) {
+      if (!exists) {
+        FirebaseAuth.instance.signOut();
+      }
+      setState(() {
+        _isLoading = false;
+      });
+    });
   }
 
   Future<void> _getUserLocation() async {
@@ -64,7 +72,7 @@ class _MapScreenState extends State<MapScreen> {
 
       _saveUserLocation();
 
-      Timer.periodic(const Duration(seconds: 30), (timer) {
+      Timer.periodic(const Duration(seconds: 10), (timer) {
         _saveUserLocation();
       });
     } catch (e) {
@@ -98,6 +106,19 @@ class _MapScreenState extends State<MapScreen> {
     }
 
     try {
+      setState(() {
+        _isLoading = true;
+        _checkIfUserExists(FirebaseAuth.instance.currentUser!.uid)
+            .then((exists) {
+          if (!exists) {
+            FirebaseAuth.instance.signOut();
+          }
+          setState(() {
+            _isLoading = false;
+          });
+        });
+      });
+
       final geoPoint =
           GeoPoint(_currentLocation.latitude, _currentLocation.longitude);
       await FirebaseFirestore.instance
@@ -114,27 +135,33 @@ class _MapScreenState extends State<MapScreen> {
 
   @override
   Widget build(BuildContext context) {
+//traer id de usuario
+
     return Scaffold(
       appBar: AppBar(
-  title: Text(
-    'Mapa BiciBuho',
-    style: TextStyle(
-      color: Colors.white, // define el color del texto en la AppBar
-      fontWeight: FontWeight.bold, // define el peso de la fuente del texto en la AppBar
-      fontSize: 20.0, // define el tamaño de la fuente del texto en la AppBar
-      fontFamily: 'Roboto', // define la fuente de la fuente del texto en la AppBar
-    ),
-  ),
-  backgroundColor: Color.fromARGB(255, 171, 206, 235), // define el color de fondo de la AppBar
-  elevation: 0, // elimina la sombra debajo de la AppBar
-  centerTitle: true, // centra el título en la AppBar
-  leading: IconButton(
-    icon: Icon(Icons.arrow_back),
-    onPressed: () {
-      Navigator.pop(context);
-    },
-  ),
-),
+        title: const Text(
+          'Mapa BiciBúho',
+          style: TextStyle(
+            color: Colors.white, // define el color del texto en la AppBar
+            fontWeight: FontWeight
+                .bold, // define el peso de la fuente del texto en la AppBar
+            fontSize:
+                20.0, // define el tamaño de la fuente del texto en la AppBar
+            fontFamily:
+                'Roboto', // define la fuente de la fuente del texto en la AppBar
+          ),
+        ),
+        backgroundColor: Color.fromARGB(
+            255, 171, 206, 235), // define el color de fondo de la AppBar
+        elevation: 0, // elimina la sombra debajo de la AppBar
+        centerTitle: true, // centra el título en la AppBar
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
       body: _isLoading
           ? Center(
               child: CircularProgressIndicator(),
@@ -149,36 +176,111 @@ class _MapScreenState extends State<MapScreen> {
                       zoom: 15,
                     ),
                     markers: snapshot.data!,
+                    onMapCreated: (GoogleMapController controller) async {
+                      await _getUserLocation();
+                      controller.animateCamera(
+                        CameraUpdate.newCameraPosition(
+                          CameraPosition(
+                            target: _currentLocation,
+                            zoom: 8,
+                          ),
+                        ),
+                      );
+                    },
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: true,
+                    onCameraMove: (position) {
+                      setState(() {
+                        _currentLocation = position.target;
+                        _checkIfUserExists(
+                                FirebaseAuth.instance.currentUser!.uid)
+                            .then((exists) {
+                          if (!exists) {
+                            AlertDialog(
+                              title: Text('Alerta'),
+                              content: Text('Su cuenta ha sido eliminada'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => StartScreen()),
+                                    );
+                                  },
+                                  child: Text('Aceptar'),
+                                ),
+                              ],
+                            );
+                            FirebaseAuth.instance.signOut();
+                          }
+                          setState(() {
+                            _isLoading = false;
+                          });
+                        });
 
-                  //  onMapCreated: (GoogleMapController controller) async {
-                  //     await _getUserLocation();
-                  //     controller.animateCamera(
-                  //       CameraUpdate.newCameraPosition(
-                  //         CameraPosition(
-                  //           target: _currentLocation,
-                  //           zoom: 8,
-                  //         ),
-                  //       ),
-                  //     );
-                  //   },
-                  //   myLocationEnabled: true,
-                  //   myLocationButtonEnabled: true,
-                  //   onCameraMove: (position) {
-                  //     setState(() {
-                  //       _currentLocation = position.target;
-                  //     });
-                  //   },
+                        //verificar si existe un usuario
 
-
-
+                        if (_markers.containsKey('me')) {
+                          _markers.remove('me');
+                        }
+                        _markers['me'] = Marker(
+                          markerId: MarkerId('me'),
+                          position: _currentLocation,
+                          infoWindow: InfoWindow(
+                            title: 'Me',
+                            snippet: _currentLocation.toString(),
+                          ),
+                        );
+                      });
+                    },
                   );
                 } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
+                  return Center(child: CircularProgressIndicator());
                 }
               },
             ),
     );
+  }
+
+  Future<bool> checkIfUserExists(String userId) async {
+    try {
+      final user = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+
+      return user.exists;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  String getUserId() {
+    return FirebaseAuth.instance.currentUser!.uid;
+  }
+
+  Future<void> _signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => const StartScreen(),
+      ),
+    );
+  }
+
+  Future<bool> _checkIfUserExists(String userId) async {
+    try {
+      final user = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .get();
+
+      return user.exists;
+    } catch (e) {
+      print(e);
+      return false;
+    }
   }
 }
